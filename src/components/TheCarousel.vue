9402e529a75af243d82ffc5c198f7b357bcce70c
gearmobile <gearmobile@gmail.com>
2018-07-16 14:29:01 +0300

    fix anv buttons in caroussel

--- src/components/TheCarousel.vue
+++ src/components/TheCarousel.vue
@@ -1,6 +1,9 @@
 <template>
   <swiper :options="swiperOption">
     <caroussel-item v-for="slide in slides" :key="slide.id" :slide="slide" />
+    <div class="swiper-pagination" slot="pagination"></div>
+    <div class="swiper-button-prev" slot="button-prev"></div>
+    <div class="swiper-button-next" slot="button-next"></div>
   </swiper>
 </template>
 
@@ -16,6 +19,14 @@ export default {
         autoHeight: true,
         slidesPerView: 3,
         spaceBetween: 10,
+        navigation: {
+          nextEl: '.swiper-button-next',
+          prevEl: '.swiper-button-prev'
+        },
+        pagination: {
+          el: '.swiper-pagination',
+          type: 'fraction'
+        },
         breakpoints: {
           768: {
             slidesPerView: 3,
@@ -25,7 +36,7 @@ export default {
             slidesPerView: 2,
             spaceBetween: 20
           },
-          320: {
+          420: {
             slidesPerView: 1,
             spaceBetween: 10
           }
@@ -46,5 +57,14 @@ export default {
 </script>
 
 <style lang="scss" scoped>
+@import '../assets/global-styles.scss';
 @import '../../node_modules/swiper/dist/css/swiper.css';
+
+@media screen and (max-width: $small) {
+  .swiper-button-prev,
+  .swiper-button-next {
+    display: none;
+  }
+}
+
 </style>
